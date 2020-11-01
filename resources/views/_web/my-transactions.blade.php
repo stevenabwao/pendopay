cd58b4a9ec722d55d0e78f8f9eb17ea32d8572f5
stevenabwao <stevefotieno@gmail.com>
2020-11-01 22:26:10 +0300

    adding transaction controller and transaction blade

---
+++ resources/views/_web/my-transactions.blade.php
@@ -0,0 +1,18 @@
+@extends('_web.layouts.master')
+
+@section('title')
+
+My-transactions
+
+@endsection
+
+
+@section('content')
+
+       <h1>here we are</h1>
+              
+      
+
+       
+@endsection
+
